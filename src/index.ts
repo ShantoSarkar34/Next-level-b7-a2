454245ce459ab303032b0b5d398a8c6559d61709
ShantoSarkar34 <shanto9070sarkar@gmail.com>
2026-05-20 20:32:58 +0600

    file serup done

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import app from "./app";
+import config from "./config";
+
+
+const port = config.port
+
+const main = async () => {
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+};
+
+main()
